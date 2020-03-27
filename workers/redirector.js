e9c2fb2bc4f6fe99e0d9d446c88e20ec6e2efca5
Alex Gibson <alexgibson@users.noreply.github.com>
2020-03-27 16:52:17 +0000

    Updating configs for current experiments

--- workers/redirector.js
+++ workers/redirector.js
@@ -18,12 +18,7 @@ const experimentPages = [
     {
         'targetPath': `/en-US/firefox/new/`,
         'sandboxPath': `/en-US/exp/firefox/new/`,
-        'sampleRate': 0.08
-    },
-    {
-        'targetPath': `/en-US/firefox/`,
-        'sandboxPath': `/en-US/exp/firefox/`,
-        'sampleRate': 0.24
+        'sampleRate': 0.12
     }
 ];
 
